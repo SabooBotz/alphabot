8d4eb6e6eeef457c486514a0dd9e1687c23f6902
SabooBotz <niketutpariani55@gmail.com>
2022-04-01 01:38:04 +0800

    alphabottt

---
+++ shop/allpayment.js
@@ -0,0 +1,26 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY SABO
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0856 9230 5785
+2. OVO
+	ㅁ Gaada
+3. DANA
+	ㅁ 0821 4520 3493
+
+*📲 PULSA*
+
+1. INDOSAT
+	ㅁ 0856 9230 5785
+2. TELKOMESEL
+    ㅁ 0821 4520 3493
+
+	
+Sebelum Melakukan Pembayaran Harap Hubungi Owner Terlebih Dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
